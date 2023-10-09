009302073cb0156f232efb9860f299800beb9acb
Hardik Kalia <hardikkalia@Hardiks-MacBook-Air.local>
2023-10-10 01:25:44 +0530

    💀

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,3 +1,7 @@
+#include <Adafruit_BMP280.h>
+
+#include <WiFi.h>
+
 #include "DHT.h"
 #include <ThingSpeak.h>
 #include <WiFi.h>
@@ -16,24 +20,26 @@ WiFiClient client;
 PubSubClient mqttClient(client);
 
 
-void mqttPublish(long pubChannelID, char* pubWriteAPIKey, float dataValue1, float dataValue2, float dataValue3) {
+void mqttPublish(long pubChannelID, char* pubWriteAPIKey, float dataValue1, float dataValue2, float dataValue3, float dataValue4) {
   String topicString = "channels/" + String(pubChannelID) + "/publish";
   String dataString = "field1=" + String(dataValue1, 2);
-  dataString+= "field2=";
+  dataString+= "&field2=";
   dataString+= String(dataValue2, 2);
-  dataString+= "field3=";
-  dataString+= String(dataValue3, 3);
+  dataString+= "&field3=";
+  dataString+= String(dataValue3, 2);
+  dataString+= "&field4=";
+  dataString+= String(dataValue4, 2);
   Serial.println(topicString);
   Serial.println(dataString);
   mqttClient.publish(topicString.c_str(), dataString.c_str());
   Serial.println(pubChannelID);
 }
 
-#define DHT_PIN 14 // Change this to the GPIO pin you connected your DHT11 sensor to
+#define DHT_PIN 12 // Change this to the GPIO pin you connected your DHT11 sensor to
 #define DHT_TYPE DHT11
 
-#define MQ2_PIN 2 // The analog input pin connected to the DOUT pin of the MQ-2 sensor
-#define MQ135_PIN 4 // The analog input pin connected to the MQ-135 sensor
+#define MQ2_PIN 32 // The analog input pin connected to the DOUT pin of the MQ-2 sensor
+#define MQ135_PIN 33 // The analog input pin connected to the MQ-135 sensor
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -42,7 +48,7 @@ void setup() {
   dht.begin();
   pinMode(MQ2_PIN, INPUT);
   pinMode(MQ135_PIN, INPUT);
-
+  WiFi.begin(ssid,password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
@@ -66,11 +72,11 @@ void loop() {
   float humidityDHT = dht.readHumidity();
 
   // Read analog value from MQ-2 gas sensor
-  int sensorValueMQ2 = analogRead(MQ2_PIN);
+  float sensorValueMQ2 = analogRead(MQ2_PIN);
   float voltageMQ2 = (sensorValueMQ2 / 4095.0) * 5.0; // Convert ADC reading to voltage
 
   // Read analog value from MQ-135 gas sensor
-  int sensorValueMQ135 = analogRead(MQ135_PIN);
+  float sensorValueMQ135 = analogRead(MQ135_PIN);
   // You can add code to interpret the sensor value from the MQ-135 here
   // Refer to the MQ-135 sensor datasheet for calibration and conversion factors
 
@@ -96,7 +102,7 @@ void loop() {
   Serial.println(sensorValueMQ135);
 
   Serial.println();
-  mqttPublish(ChannelID, APIKey, temperatureDHT, humidityDHT, sensorValueMQ2);
+  mqttPublish(ChannelID, APIKey, temperatureDHT, humidityDHT, sensorValueMQ2, sensorValueMQ135);
   delay(2000); // Wait for 2 seconds before taking the next reading
 
 }
