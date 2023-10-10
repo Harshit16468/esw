bb3959b1aaa0992de1f2b727b96f252abd20d5bf
Hardik Kalia <hardikkalia@Hardiks-MacBook-Air.local>
2023-10-10 15:56:21 +0530

    bmp

--- Code/Code.ino
+++ Code/Code.ino
@@ -19,8 +19,11 @@ const char* clientID = "DzkQOw0wMRUeBREaMQE7Hg0";
 WiFiClient client;
 PubSubClient mqttClient(client);
 
+Adafruit_BMP280 bmp;
 
-void mqttPublish(long pubChannelID, char* pubWriteAPIKey, float dataValue1, float dataValue2, float dataValue3, float dataValue4) {
+
+
+void mqttPublish(long pubChannelID, char* pubWriteAPIKey, float dataValue1, float dataValue2, float dataValue3, float dataValue4,float data5,float data6) {
   String topicString = "channels/" + String(pubChannelID) + "/publish";
   String dataString = "field1=" + String(dataValue1, 2);
   dataString+= "&field2=";
@@ -29,6 +32,10 @@ void mqttPublish(long pubChannelID, char* pubWriteAPIKey, float dataValue1, floa
   dataString+= String(dataValue3, 2);
   dataString+= "&field4=";
   dataString+= String(dataValue4, 2);
+  dataString+= "&field5=";
+  dataString+= String(data5, 2);
+  dataString+= "&field6=";
+  dataString+= String(data6, 2);
   Serial.println(topicString);
   Serial.println(dataString);
   mqttClient.publish(topicString.c_str(), dataString.c_str());
@@ -53,12 +60,17 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
+  
 
   Serial.print("Local IP address: ");
   Serial.println(WiFi.localIP());
   mqttClient.setServer(server, 1883);
+  bmp.begin(0x76,0x58);
 }
 
+int temp_var=0;
+int mq2_var=0;
+int mq135_var=0;
 void loop() {
 
   if (!mqttClient.connected()) {
@@ -74,12 +86,14 @@ void loop() {
   // Read analog value from MQ-2 gas sensor
   float sensorValueMQ2 = analogRead(MQ2_PIN);
   float voltageMQ2 = (sensorValueMQ2 / 4095.0) * 5.0; // Convert ADC reading to voltage
+  sensorValueMQ2/=10;
+
 
   // Read analog value from MQ-135 gas sensor
   float sensorValueMQ135 = analogRead(MQ135_PIN);
   // You can add code to interpret the sensor value from the MQ-135 here
   // Refer to the MQ-135 sensor datasheet for calibration and conversion factors
-
+  sensorValueMQ135/=2;
   // Print DHT11 data
   Serial.println("DHT11 Sensor Data:");
   Serial.print("Temperature: ");
@@ -101,8 +115,51 @@ void loop() {
   Serial.print("Sensor Value: ");
   Serial.println(sensorValueMQ135);
 
+  /// BME
+
+  float pressure = bmp.readPressure();
+  float alt = bmp.readAltitude();
+
+  Serial.println("BMP:");
+  Serial.print("Pressure: ");
+  Serial.println(pressure);
+Serial.print("Altitude: ");
+  Serial.println(alt);  
+
+
+
+  if(temperatureDHT>45)
+  temp_var++;
+  else 
+  temp_var=0;
+  if(sensorValueMQ2>300)
+  mq2_var++;
+  else
+  mq2_var=0;
+  if(sensorValueMQ135>1500)
+  mq135_var++;
+  else
+  mq135_var=0;
+  
+  if(temp_var>=5 )
+  {
+    Serial.println("Abnormal Temperatur");
+    delay(1000);    
+  }
+  if(mq2_var>=5 )
+  {
+    Serial.println("Smoke Detected");
+    delay(1000);    
+  }
+  if(mq135_var>=5)
+  {
+    Serial.println("Abnormal Co2 levels");
+    delay(1000);    
+
+  }
+
   Serial.println();
-  mqttPublish(ChannelID, APIKey, temperatureDHT, humidityDHT, sensorValueMQ2, sensorValueMQ135);
+  mqttPublish(ChannelID, APIKey, temperatureDHT, humidityDHT, sensorValueMQ2, sensorValueMQ135,pressure,alt);
   delay(2000); // Wait for 2 seconds before taking the next reading
 
 }
